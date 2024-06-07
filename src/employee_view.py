4d0ae77a681a21690efcf1579fb37fce628a7381
Raden Wisnu Andhika Pranidhia <radenwisnu21@gmail.com>
2024-06-07 20:14:51 +0700

    feat: add swagger for employees api

--- src/employee_view.py
+++ src/employee_view.py
@@ -5,6 +5,7 @@ from flasgger import swag_from
 employees_blueprint = Blueprint("employees", __name__)
 
 @employees_blueprint.route("/employees", methods=["POST"])
+@swag_from("docs/register_employee.yml")
 def register_employee():
     name = request.json.get("name")
     email = request.json.get("email")
@@ -28,18 +29,23 @@ def register_employee():
     return jsonify({"message": "employee created"}), 201
 
 @employees_blueprint.route("/employees", methods=["GET"])
+@swag_from("docs/list_employee.yml")
 def get_employees():
+    if len(employees) == 0:
+        return jsonify({"error": "employee list is empty!"}), 404
     return jsonify(employees), 200
 
 @employees_blueprint.route("/employees/<int:employee_id>", methods=["GET"])
+@swag_from("docs/get_employee.yml")
 def get_employee(employee_id):
     for employee in employees:
         if employee.get("id") == employee_id:
             return jsonify(employee), 200
         
-    return jsonify({"error": "employee not found!"}), 400
+    return jsonify({"error": "employee not found!"}), 404
 
 @employees_blueprint.route("/employees/<int:employee_id>", methods=["PUT"])
+@swag_from("docs/update_employee.yml")
 def update_employee(employee_id):
     for employee in employees:
         if employee.get("id") == employee_id:
@@ -50,13 +56,14 @@ def update_employee(employee_id):
             employee["schedule"] = request.json.get("schedule", employee["schedule"])
             return jsonify(employee), 200
     
-    return jsonify({"error": "employee not found!"}), 400
+    return jsonify({"error": "employee not found!"}), 404
 
 @employees_blueprint.route("/employees/<int:employee_id>", methods=["DELETE"])
+@swag_from("docs/delete_employee.yml")
 def delete_employee(employee_id):
     for employee in employees:
         if employee.get("id") == employee_id:
             employees.remove(employee)
-            return jsonify({"result": True}), 200
+            return jsonify({"message": "employee deleted succesfully"}), 200
         
-    return jsonify({"error": "employee not found!"}), 400
+    return jsonify({"error": "employee not found!"}), 404
