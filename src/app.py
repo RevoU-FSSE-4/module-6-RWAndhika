9da1c2cba9b7f08c36d145c1a5de82f394191187
Raden Wisnu Andhika Pranidhia <radenwisnu21@gmail.com>
2024-06-14 18:07:21 +0700

    fix: folder route for blueprint

--- src/app.py
+++ src/app.py
@@ -1,17 +1,12 @@
 from flask import Flask
-from flasgger import Swagger
 from blog.routes.employees_routes import employees_blueprint
 from blog.routes.animals_routes import animals_blueprint
-# from employee_view import employees_blueprint
-# from animal_view import animals_blueprint
+from flasgger import Swagger
 
 app = Flask(__name__)
-swagger = Swagger(app)
-
 app.register_blueprint(employees_blueprint)
 app.register_blueprint(animals_blueprint)
-# app.register_blueprint(employees_blueprint)
-# app.register_blueprint(animals_blueprint)
+swagger = Swagger(app)
 
 if __name__ == "__main__":
     app.run(debug=True)
